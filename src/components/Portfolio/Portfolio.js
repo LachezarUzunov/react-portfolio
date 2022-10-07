d12c7f15c062e300569007d92375f53a365302d6
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-10-08 02:20:37 +0300

    Adding testimonials and swiper js

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,165 @@
-import classes from './Portfolio.module.css'
+import classes from "./Portfolio.module.css";
+import img1 from "../../assets/portfolio1.jpg";
+import img2 from "../../assets/portfolio2.jpg";
+import img3 from "../../assets/portfolio3.jpg";
+import img4 from "../../assets/portfolio4.jpg";
+import img5 from "../../assets/portfolio5.png";
+import img6 from "../../assets/portfolio6.jpg";
 
 const Portfolio = () => {
-    return <section id='portfolio'>Portfolio</section>
-}
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className={`${"container"} ${classes.portfolio__container}`}>
+        <article className={classes.portfolio__item}>
+          <div className={classes.portfolio__item_image}>
+            <img src={img1} alt="portfolio1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className={classes.portfolio__item_cta}>
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className={classes.portfolio__item}>
+          <div className={classes.portfolio__item_image}>
+            <img src={img1} alt="portfolio1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className={classes.portfolio__item_cta}>
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className={classes.portfolio__item}>
+          <div className={classes.portfolio__item_image}>
+            <img src={img1} alt="portfolio1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className={classes.portfolio__item_cta}>
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className={classes.portfolio__item}>
+          <div className={classes.portfolio__item_image}>
+            <img src={img1} alt="portfolio1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className={classes.portfolio__item_cta}>
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className={classes.portfolio__item}>
+          <div className={classes.portfolio__item_image}>
+            <img src={img1} alt="portfolio1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className={classes.portfolio__item_cta}>
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className={classes.portfolio__item}>
+          <div className={classes.portfolio__item_image}>
+            <img src={img1} alt="portfolio1" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className={classes.portfolio__item_cta}>
+            <a
+              href="https://github.com"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,7 +1,92 @@
-import classes from './Testimonials.module.css';
+import classes from "./Testimonials.module.css";
+import avt1 from "../../assets/avatar1.jpg";
+import avt2 from "../../assets/avatar2.jpg";
+import avt3 from "../../assets/avatar3.jpg";
+import avt4 from "../../assets/avatar4.jpg";
+
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
-    return <section id='testimonials'>Testimonials</section>
-}
+  return (
+    <section id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className={`${"container"} ${classes.testimonials__container}`}
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide className={classes.testimonials}>
+          <div className={classes.client__avatar}>
+            <img src={avt1} alt="avatar1" />
+          </div>
+          <h5 className={classes.client__name}>Jamie Oliver</h5>
+          <small className={classes.client__review}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className={classes.testimonials}>
+          <div className={classes.client__avatar}>
+            <img src={avt1} alt="avatar1" />
+          </div>
+          <h5 className={classes.client__name}>Jamie Oliver</h5>
+          <small className={classes.client__review}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className={classes.testimonials}>
+          <div className={classes.client__avatar}>
+            <img src={avt1} alt="avatar1" />
+          </div>
+          <h5 className={classes.client__name}>Jamie Oliver</h5>
+          <small className={classes.client__review}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className={classes.testimonials}>
+          <div className={classes.client__avatar}>
+            <img src={avt1} alt="avatar1" />
+          </div>
+          <h5 className={classes.client__name}>Jamie Oliver</h5>
+          <small className={classes.client__review}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </small>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
 
 export default Testimonials;
